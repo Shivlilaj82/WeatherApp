763e24d66e4f31bad97c6964d603b163503b55aa
Shivlila jatte <122195923+Shivlilaj82@users.noreply.github.com>
2024-10-28 14:08:23 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "802b3f81d480129a69818b3d85fb1d04",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
